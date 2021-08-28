259f2f220e4f0b543efe7f202e4c2f63e1d244a9
Richard <82630326+richardsavala@users.noreply.github.com>
2021-08-27 22:22:27 -0600

    Add files via upload
    react portfolio
    in App.js App return added Navbar, About, Projects, Skills, Testimonials, and Contact

--- src/App.js
+++ src/App.js
@@ -7,5 +7,14 @@ import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
 export default function App() {
-    return "App";
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
