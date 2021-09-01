64c6d7297f460e8bb8ada6d130afdba14fed0965
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-01 07:03:35 -0600

    Add files via upload
    react portfolio

---
+++ Projects.js
@@ -0,0 +1,15 @@
+import { CodeIcon } from "@heroicons/react/solid";
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+  return (
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div>
+        <div className="">
+          <CodeIcon />
+        </div>
+      </div>
+    </section>
+  );
+}
