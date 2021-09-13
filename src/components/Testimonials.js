cc80fa85e9b7d2d581f9d815cff8ae3b8bef9ef9
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-12 22:48:42 -0600

    Add files via upload
    react portfolio
    in Testimonials.js added return ()

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,4 +2,20 @@ import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "../data";
 
-export default function Testimonials() {}
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
+      <div className="conatiner px-5 py-10 mx-auto text-center">
+        <UsersIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Client Testimonials
+        </h1>
+        <div className="flex flex-wrap m-4">
+          {testimonials.map((testimonial) => (
+            <div className="p-4 md:w-1/2 w-full"></div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
