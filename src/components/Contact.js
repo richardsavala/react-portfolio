655ebe6d87fb3078a3d25a8d795c42dd527aec1e
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-17 23:59:34 -0600

    Add files via upload
    react portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,33 @@
-export default function Contact() {}
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto lfex sm:flex-nowrap flex-wrap">
+        <div
+          className="g:w-2/3 md:w1/2 bg-gray- 900 rounded-lg overflow-hidden
+        sm:mr-10 p-10 flex items end justify-start relative"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2960899.6256571757!2d-118.56711054951474!3d43.55438970624503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54afb2bb781a17cb%3A0x6edc428b43754d9c!2sCaldwell%2C%20ID!5e0!3m2!1sen!2sus!4v1631943112198!5m2!1sen!2sus"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
