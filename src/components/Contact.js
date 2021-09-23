6225c3707106320ecc80900b55b38ba0825ace47
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-22 20:54:28 -0600

    Add files via upload
    reacrt portfolio - contact submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -61,6 +61,7 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => SVGAnimateTransformElement(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
