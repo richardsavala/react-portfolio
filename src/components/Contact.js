f27b50a4411746133b81ad02cbe8f3bbbda9fb76
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-23 23:01:27 -0600

    Add files via upload
    react portfolio - netlify
    in Contact.js added function handleSubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,6 +14,17 @@ export default function Contact() {
     );
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "applicationx-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
