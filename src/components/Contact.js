77dced01033f2bee1adb8e8dd025e6b9a17d77a4
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-25 16:11:29 -0600

    Add files via upload
    react portfolio - netlify
    corrected errors in Contact.js, 
    in Contact.js form, added onSubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,19 +6,18 @@ export default function Contact() {
   const [message, setMessage] = React.useState("");
 
   function encode(data) {
-    return (
-      Object.keys(data).map(
+    return Object.keys(data)
+      .map(
         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      ),
-      join("&")
-    );
+      )
+      .join("&");
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/", {
       method: "POST",
-      headers: { "Content-Type": "applicationx-www-form-urlencoded" },
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
       .then(() => alert("Message sent!"))
@@ -67,6 +66,7 @@ export default function Contact() {
         <form
           netlify
           name="contact"
+          onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -85,7 +85,7 @@ export default function Contact() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => SVGAnimateTransformElement(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -97,6 +97,7 @@ export default function Contact() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -110,6 +111,7 @@ export default function Contact() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
