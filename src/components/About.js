a9e31d4bd87d94026078c0ba490bbf19119fae52
Richard <82630326+richardsavala@users.noreply.github.com>
2021-09-26 15:34:36 -0600

    Add files via upload
    react portfolio - about page button fix
    in About.js  updated buttons

--- src/components/About.js
+++ src/components/About.js
@@ -17,18 +17,15 @@ export default function About() {
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
-              Work
-              with
-              me
-            ></a>
+            >
+              Work with me
+            </a>
             <a
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
-              See
-              my
-              past
-              work
-            ></a>
+            >
+              See my work
+            </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2.w-5/6">
