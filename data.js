af9ee173700c55380ff26eab85e1ff3034eca261
Richard <82630326+richardsavala@users.noreply.github.com>
2021-10-05 17:58:15 -0600

    Add files via upload
    updated data in data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+export const projects = [{
+        title: "React Advanced Tutorial",
+        subtitle: "React Cart",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://github.com/richardsavala/advanced-react-tutorial",
+    },
+    {
+        title: "React Advanced Tutorial",
+        subtitle: "React Stripe Menus",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./stripe.gif",
+        link: "https://github.com/richardsavala/react-stripe-menus",
+    },
+    {
+        title: "React Advanced Tutorial",
+        subtitle: "React Sidebar Modal",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./sidebar-modal.gif",
+        link: "https://github.com/richardsavala/react-sidebar-modal",
+    },
+    {
+        title: "React Advanced Tutorial",
+        subtitle: "React List",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./grocery-bud.gif",
+        link: "https://github.com/richardsavala/react-navbar",
+    },
+];
+
+export const testimonials = [{
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jesse Hicks",
+        company: "Zoozle",
+    },
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+        image: "https://randomuser.me/api/portraits/men/94.jpg",
+        name: "Ruben Alvarez",
+        company: "Dooble.io",
+    },
+];
+
+export const skills = ["JavaScript", "Tailwind CSS", "React"];
