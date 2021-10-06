e287899bea4cf24ec4eba8b2e8002897a0ed689a
Richard <82630326+richardsavala@users.noreply.github.com>
2021-10-05 19:48:51 -0600

    Deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,14 +43,11 @@ export default function Contact() {
             src="https://www.google.com/maps/embed/v1/place?q=caldwell,+ID&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
-              </h2>
-              <p className="mt-1">Caldwell, ID 83605</p>
-            </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                Contact Me:
+              </h2>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
@@ -60,6 +57,12 @@ export default function Contact() {
                 PHONE
               </h2>
               <p className="leading-relaxed">208-965-5450</p>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                Github
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                https://github.com/richardsavala
+              </a>
             </div>
           </div>
         </div>
@@ -73,8 +76,7 @@ export default function Contact() {
             Hire Me
           </h2>
           <p className="leading-relxed mb-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-            eligendi mollitia consequuntur quasi error quibusdam!
+            I built this 'Contact Me' app with a Netlify API, check it out!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
