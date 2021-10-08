ffcd32f83a5e2ae16728dc6a2b102a0f6f308b18
Richard <82630326+richardsavala@users.noreply.github.com>
2021-10-07 22:33:56 -0600

    first ccommit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ export default function Projects() {
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            This portfolio website is my latest project, here are a few of my
-            past projects.
+            This portfolio website is my latest project, but here are a few of
+            my past projects.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
